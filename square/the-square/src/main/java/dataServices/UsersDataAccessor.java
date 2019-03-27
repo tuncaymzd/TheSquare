928e7f0be3bc59540f99ecf3b9c90f4934fb5ed6
Turkoglu Tuncay <tuncay.turkoglu@etudiant.univ-lille1.fr>
2019-03-28 00:51:19 +0100

    ajout de readAll

--- square/the-square/src/main/java/dataServices/UsersDataAccessor.java
+++ square/the-square/src/main/java/dataServices/UsersDataAccessor.java
@@ -3,6 +3,7 @@ package dataServices;
 import model.Users;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -31,7 +32,34 @@ public class UsersDataAccessor implements IDataAccessor<Users> {
 
     @Override
     public ArrayList<Users> readAll() {
-        return null;
+        ArrayList<Users> usersList = new ArrayList<Users>();
+        try{
+            MySQLConnection.getInstance().Connection();
+            Connection connection = MySQLConnection.getInstance().getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet set = statement.executeQuery("SELECT * From users;");
+            while (set.next()){
+                Users usersTmp = null;
+                usersTmp = new Users.Builder().Build();
+                usersTmp.setId(set.getInt(1));
+                usersTmp.setFirstname(set.getString(2));
+                usersTmp.setLastname(set.getString(3));
+                usersTmp.setBirthday(set.getString(4));
+                usersTmp.setCity(set.getString(5));
+                usersTmp.setMail(set.getString(6));
+                usersTmp.setPhone(set.getString(7));
+                usersTmp.setDescription(set.getString(8));
+                usersList.add(usersTmp);
+            }
+            set.close();
+            statement.close();
+        } catch (SQLException e){
+            System.out.println("Error occured while getting this users");
+            e.printStackTrace();
+        } finally {
+            MySQLConnection.getInstance().disconnect();
+        }
+        return usersList;
     }
 
     @Override
